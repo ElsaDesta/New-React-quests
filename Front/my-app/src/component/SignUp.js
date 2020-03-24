4fec9fab1dfe3467e0b8d3b6f32f354405082384
Elsa Desta <elsamenelik@gmail.com>
2020-03-24 14:06:20 +0100

    quest020

--- Front/my-app/src/component/SignUp.js
+++ Front/my-app/src/component/SignUp.js
@@ -1,33 +1,67 @@
-
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class SignUp extends Component {
-    state= {
-        email: ""
-    }
+  state = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  };
 
-    updateEmailField = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    };
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-      
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-    render  () {
-        return (
-            <div>
-                new component
-              <input 
-              placeholder="test@test.com" 
-              value = {this.state.email}
-              onChange = {e=> this.updateEmailField(e)}
-              type="email" name="email" />
-            </div>
-        );
-    }
+  render() {
+    const List = JSON.stringify(this.state);
+    return (
+      <div>
+        <form>
+          <h1>{List}</h1>
+          <input
+            placeholder="test@test.com"
+            value={this.state.email}
+            onChange={e => this.change(e)}
+            type="email"
+            name="email"
+          />{" "}
+          <br></br>
+          <input
+            placeholder="password"
+            value={this.state.password}
+            onChange={e => this.change(e)}
+            type="password"
+            name="password"
+          />{" "}
+          <br></br>
+          <input
+            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={e => this.change(e)}
+            type="text"
+            name="firstName"
+          />{" "}
+          <br></br>
+          <input
+            placeholder="Last Name"
+            value={this.state.lastName}
+            onChange={e => this.change(e)}
+            type="text"
+            name="lastName"
+          />{" "}
+          <br></br>
+          <button onClick={e => this.onSubmit(e)}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-
 
 export default SignUp;
